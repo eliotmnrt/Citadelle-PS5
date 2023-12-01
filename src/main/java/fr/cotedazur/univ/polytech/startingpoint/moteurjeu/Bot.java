d13a8a342e864a5bcfc23e41cb495f41306920c5
Gwendolyne Bouchard <gwendolyne.bouchard@etu.univ-cotedazur.fr>
2023-12-01 16:53:52 +0100

    #1 début BOT et Jeu

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/moteurjeu/Bot.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/moteurjeu/Bot.java
@@ -3,9 +3,32 @@ package fr.cotedazur.univ.polytech.startingpoint.moteurjeu;
 import fr.cotedazur.univ.polytech.startingpoint.cartes.Quartier;
 import fr.cotedazur.univ.polytech.startingpoint.cartes.Role;
 
+import java.util.ArrayList;
+
 public class Bot {
     int nbOr;
     Role role;
-    Quartier[] quartierConstruit;
-    Quartier[] quartierMain;
+    ArrayList<Quartier> quartierConstruit;
+    ArrayList<Quartier> quartierMain;
+    public Bot(){
+        nbOr = 0;
+        // mettre role = roi
+        quartierConstruit = new ArrayList<Quartier>();
+        quartierMain = new ArrayList<Quartier>();
+    }
+    public int getOr(){
+        return nbOr;
+    }
+    public void setOr(int or){
+        nbOr = or;
+    }
+    public void setRole(Role role){
+        this.role = role;
+    }
+    public void ajoutQuartierConstruit(Quartier newQuartier){
+        quartierConstruit.add(newQuartier);
+    }
+    public void ajoutQuartierMain(Quartier newQuartier){
+        quartierMain.add(newQuartier);
+    }
 }
--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/moteurjeu/Tour.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/moteurjeu/Tour.java
@@ -1,4 +1,7 @@
 package fr.cotedazur.univ.polytech.startingpoint.moteurjeu;
 
 public class Tour {
+    //avoir une liste de tout les perso
+    //génerer aléatoirement une liste de nombre de BOT +1
+
 }
