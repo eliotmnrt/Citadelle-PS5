49efe3325f1b8f776bb697726c271a8dfac63bf5
Gwendolyne Bouchard <gwendolyne.bouchard@etu.univ-cotedazur.fr>
2023-12-01 16:35:15 +0100

    #1 ajout enum Quartier et classe Bot

---
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/cartes/Quartier.java
@@ -0,0 +1,4 @@
+package fr.cotedazur.univ.polytech.startingpoint.cartes;
+
+public enum Quartier {
+}
---
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/moteurjeu/Bot.java
@@ -0,0 +1,11 @@
+package fr.cotedazur.univ.polytech.startingpoint.moteurjeu;
+
+import fr.cotedazur.univ.polytech.startingpoint.cartes.Quartier;
+import fr.cotedazur.univ.polytech.startingpoint.cartes.Role;
+
+public class Bot {
+    int nbOr;
+    Role role;
+    Quartier[] quartierConstruit;
+    Quartier[] quartierMain;
+}
