23163acae98fcaad55b75dc224d21a1991c4884a
Eliot Menoret <eliot.mnrt@gmail.com>
2023-12-28 16:23:49 +0100

    #39 ajout mockito dans le pom.xml

--- src/test/java/Citadelle/teamU/cartes/roles/MagicienTest.java
+++ src/test/java/Citadelle/teamU/cartes/roles/MagicienTest.java
@@ -1,4 +1,25 @@
+package Citadelle.teamU.cartes.roles;
+import Citadelle.teamU.moteurjeu.bots.BotAleatoire;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+import Citadelle.teamU.cartes.roles.*;
+import Citadelle.teamU.moteurjeu.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.mockito.Mockito;
+import org.mockito.Mockito.*;
+
+
 class MagicienTest {
-  
+
+    @Test
+    void changeAvecPioche() {
+        Mockito.spy(new BotAleatoire());
+    }
+
+    @Test
+    void changeAvecBot() {
+    }
 }
