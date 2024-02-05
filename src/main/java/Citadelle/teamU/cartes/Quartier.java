b636885dd279e5363e0a95d842fad2e6543aeada
Gwendolyne Bouchard <gwendolyne.bouchard@etu.univ-cotedazur.fr>
2024-02-05 15:25:33 +0100

    #69 colors

--- src/main/java/Citadelle/teamU/cartes/Quartier.java
+++ src/main/java/Citadelle/teamU/cartes/Quartier.java
@@ -19,13 +19,12 @@ public enum Quartier implements Comparable<Quartier> {
     private final int cout;
     private final TypeQuartier typeQuartier;
     private  int nbCartes;
-    public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_YELLOW = "\u001B[33m";
     public static final String ANSI_BLUE = "\u001B[34m";
     public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_WHITE = "\u001B[37m";
+    public static final String ANSI_WHITE = "\033[0;37m";
 
     Quartier(String nom, int cout, TypeQuartier typeQuartier, int nbCartes){
         this.nom = nom;
