a6d84b0a6978d411214a6f979b595b27b3d5cf9e
Gwendolyne Bouchard <gwendolyne.bouchard@etu.univ-cotedazur.fr>
2024-02-08 10:35:19 +0100

    #88 javadoc construit vite et chère

--- src/main/java/Citadelle/teamU/moteurJeu/bots/malin/BotConstruitVite.java
+++ src/main/java/Citadelle/teamU/moteurJeu/bots/malin/BotConstruitVite.java
@@ -9,30 +9,39 @@ import Citadelle.teamU.moteurJeu.bots.Bot;
 
 import java.util.*;
 
+/**
+ * La stratégie est de construit vite des quartier pas chère (< 3) pour essayer de finir en premier
+ */
 public class BotConstruitVite extends BotMalin {
     private static int numDuBot = 1;
 
+    /**
+     * @param pioche
+     */
     public BotConstruitVite(Pioche pioche){
-        //Bot qui construit le plus vite possible
-        //Il construit des qu'il peut (le moins chere)
-        //prend des piece si : il a des quartier qui coute moins de 3
-        //pioche sinon
-        //Si il ne peut pas construire : il pioche jusqu'à avoir des carte qui coute moins de 3
-        //Si il a des cartes qui coute moins de 3 : il prend de l'or
-        //Il prend l'architecte si possible
         super(pioche);
         this.name = "Bot qui construit vite_" + numDuBot;
         numDuBot++;
     }
+
+    /**
+     * Il vise de choisir l'Architecte pour construire le plus de quartiers possible rapidement
+     * @param roles
+     */
     @Override
     public void choisirRole(List<Role> roles){
         if (orProchainTour >= 0) nbOr += orProchainTour;
-        if (trouverRole(roles, "Architecte")){return;} //on cherche l architecte pour construire la max
+        if (trouverRole(roles, "Architecte")){return;}
         int intAleatoire= randInt(roles.size());
         setRole(roles.remove(intAleatoire));
         rolesRestants = new ArrayList<>(roles);
     }
 
+    /**
+     * Si il a des cartes qui coute moins de 3 : il prend de l'or
+     * Sinon il pioche et choisi la carte la moins chère
+     * @return
+     */
     @Override
     public List<Quartier> faireActionDeBase(){
         quartiersViolets();         //actions spéciales violettes
@@ -63,6 +72,10 @@ public class BotConstruitVite extends BotMalin {
         return choixDeBase;
     }
 
+    /**
+     * Construit le moins chère (qui doit couter moins de 3)
+     * @return
+     */
     @Override
     public Quartier construire(){
         List<Quartier> quartiersTrie = new ArrayList<>(quartierMain);
@@ -76,6 +89,11 @@ public class BotConstruitVite extends BotMalin {
         return null;
     }
 
+    /**
+     * @param quartierPioches liste de Quartiers piochés
+     * On choisit celui qui coute le moins chère
+     * @return
+     */
     @Override
     public List<Quartier> choisirCarte(List<Quartier> quartierPioches) {
         if (!quartierConstruit.contains(Quartier.BIBLIOTHEQUE)){
@@ -104,9 +122,12 @@ public class BotConstruitVite extends BotMalin {
     }
 
 
+    /**
+     * detruit que le quartier le moins chère du bot qui a le plus de quartier construits
+     * @param condottiere Role condottiere
+     */
     @Override
     public void actionSpecialeCondottiere(Condottiere condottiere){
-        // detruit que le quartier le moins chère du bot qui a le plus de quartier construits
         List<Bot> botList = new ArrayList<>(condottiere.getBotListe());
         botList.remove(this);
         Bot botMax = botList.get(0);
@@ -130,9 +151,17 @@ public class BotConstruitVite extends BotMalin {
         }
     }
 
+    /**
+     * Roles visible au début du tour
+     * @param rolesVisible
+     */
     public void setRolesVisible(List<Role> rolesVisible) {
         this.rolesVisible = rolesVisible;
     }
+
+    /**
+     * @return Bot construit vite_son numéro
+     */
     @Override
     public String toString(){
         return name;
